cf34652740bc1e6339d6c05cac5290d688313103
0xOthmane <0xothodor@proton.me>
2025-01-01 14:21:19 +0100

    feat: setup authentication

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -2,3 +2,5 @@ export * from './database/abstract.schema';
 export * from './database/database.module';
 export * from './rmq/rmq.service';
 export * from './rmq/rmq.module';
+export * from './auth/jwt-auth.guard';
+export * from './auth/auth.module';
